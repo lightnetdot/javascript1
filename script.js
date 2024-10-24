e96a52af26a38d52ab7342c2240f5b9390321804
lightnetdot <itsdaison3@gmail.com>
2024-10-24 19:46:18 +0400

    java-script1

---
+++ script.js
@@ -0,0 +1,16 @@
+let firstName = "Damir";
+let lastName = "Bratcev";
+let age = 16;
+let email = "itsdaison3@gmail.com";
+let phoneNumber = "568175484SA";
+let isAdult = age >= 18;
+
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Email:", email);
+
+console.log(`%cPhone Number: ${phoneNumber}`, "color: red");
+
+console.log(`%cIs Adult: ${isAdult}`, "color: yellow");
